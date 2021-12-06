f568acedb3b9101cc5d9c5b479465aab7bbb6e91
SHAINA YU <shaina.yu.iics@ust.edu.ph>
2021-12-06 10:32:43 +0800

    Finished Developers activity but without sources

--- app/src/main/java/com/cabel/ducatusapp/Developers.java
+++ app/src/main/java/com/cabel/ducatusapp/Developers.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Developers extends AppCompatActivity {
 
@@ -14,19 +15,19 @@ public class Developers extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developers);
 
-        if(SharedPrefs.getCurrentUserId(Developers.this) == null) {
-            startActivity(new Intent(Developers.this, LogIn.class));
-        }
+        //if(SharedPrefs.getCurrentUserId(Developers.this) == null) {
+        //    startActivity(new Intent(Developers.this, LogIn.class));
+        //}
 
-        if(!SharedPrefs.getUsertype(Developers.this).equals("admin")) {
-            startActivity(new Intent(Developers.this, LogIn.class));
-        }
+        //if(!SharedPrefs.getUsertype(Developers.this).equals("admin")) {
+        //    startActivity(new Intent(Developers.this, LogIn.class));
+        //}
 
         Button btnBackDevs = (Button) findViewById(R.id.btnBackDevs);
         btnBackDevs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Developers.this, Settings.class);
+                Intent intent = new Intent(Developers.this, SettingsAdmin.class);
                 startActivity(intent);
             }
         });
